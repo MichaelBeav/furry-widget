e94334cf1663d59da39085ec5f0d7c446738828a
filipovskii_off <filipovskii.off@gmail.com>
2012-12-05 15:39:59 +0400

    Ad limit changed.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express()
   .get('/api/ads', function (req, res, next) {
     withCollection('offers', function (err, collection) {
       if (err) return next(err);
-      collection.find().limit(5).toArray(function (err, arr) {
+      collection.find().limit(20).toArray(function (err, arr) {
         if (err) return next(err);
         res.header({
           'Content-Type': 'application/json; charset=utf-8'
